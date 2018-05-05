42bf5a8ed299a19c1438134361c32f718eba9be2
vestige <vestige_sync@me.com>
2018-05-05 11:26:30 +0900

    Test Interval

--- spike/it.js
+++ spike/it.js
@@ -1,7 +1,28 @@
+let remain = 2
+let start = 0
+let timer_id = 0
+
+function timedisp() {
+  document.close()
+  var current = new Date()
+  document.writeln(current.toLocaleTimeString())
+  document.write("<br />")
+  document.writeln(start.toLocaleTimeString())
+
+  document.write("<br />")
+  
+  var diff = (current.getTime() - start.getTime()) / 1000
+  document.writeln(diff)
+
+  if (diff > 10) {
+    clearInterval(timer_id)
+    document.write("<br />")
+    document.write("Finish")
+  }  
+}
+
+
 window.onload = function() {
-  window.setInterval(function() {
-    document.close()
-    var dd = new Date();
-    document.writeln(dd.toLocaleString())
-  }, 1000);
+  start = new Date()
+  timer_id = window.setInterval('timedisp()', 1000);
 }
